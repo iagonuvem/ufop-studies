1c50f5b4fa2082e5b248b19c56d0de0dffd739ae
Iago Nuvem <iagonuvem@hotmail.com>
2017-06-01 19:03:12 -0300

    reordering

---
+++ prog-1/class-29-05-2017/exercise-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+// raiz quadrada em c = sqrt()
+
+// Heron Formula's exercise
+int main()
+{
+	/* code */
+	int a, b, c, d ,p;
+
+	p = a+b+c/2;
+
+	if(sqrt(p * (p-a) * (p-b) * (p-c)) > 0){ // Se Maior que 0, é possivel formar um triangulo
+
+	}
+
+	return 0;
+}
