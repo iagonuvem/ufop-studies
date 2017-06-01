7ab9ef010aa6bd402df09dc072f9ed3e31c4de63
Iago Nuvem <iagonuvem@hotmail.com>
2017-06-01 19:15:34 -0300

    herons formula exercise

--- prog-1/class-29-05-2017/exercise-3.c
+++ prog-1/class-29-05-2017/exercise-3.c
@@ -6,13 +6,42 @@
 // Heron Formula's exercise
 int main()
 {
-	/* code */
 	int a, b, c, d ,p;
+
+	printf("Digite um lado\n");
+	scanf("%d" , &a);
+	printf("Digite um lado\n");
+	scanf("%d" , &b);
+	printf("Digite um lado\n");
+	scanf("%d" , &c);
+	printf("Digite um lado\n");
+	scanf("%d" , &d);
 
 	p = a+b+c/2;
 
 	if(sqrt(p * (p-a) * (p-b) * (p-c)) > 0){ // Se Maior que 0, é possivel formar um triangulo
+		printf("S/n");
+	}
 
+	p = a+b+d/2;
+
+	if(sqrt(p * (p-a) * (p-b) * (p-d)) > 0){
+		printf("S/n");
+	}
+
+	p = a+c+d/2;
+
+	if(sqrt(p * (p-a) * (p-c) * (p-d)) > 0){
+		printf("S/n");
+	}
+
+	p = c+b+d/2;
+
+	if(sqrt(p * (p-c) * (p-b) * (p-d)) > 0){
+		printf("S/n");
+	}
+	else{
+		printf("N\n");
 	}
 
 	return 0;
