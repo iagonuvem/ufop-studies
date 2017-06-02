70eea0cb5c0f476f6a52a7d94af6fa335a28b567
Iago Nuvem <iagonuvem@hotmail.com>
2017-06-02 11:51:18 -0300

    class-01-06

---
+++ prog-1/class-01-06-2017/exercise-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// loop structures
+int main()
+{
+	//The program will never stop (infinite loop)
+	while(1){
+		printf("loop infinito\n");
+	}
+	
+	return 0;
+}
--- prog-1/class-29-05-2017/exercise-3.c
+++ prog-1/class-29-05-2017/exercise-3.c
@@ -8,40 +8,49 @@ int main()
 {
 	int a, b, c, d ,p;
 
-	printf("Digite um lado\n");
+	printf("Digite um lado A\n");
 	scanf("%d" , &a);
-	printf("Digite um lado\n");
+	printf("Digite um lado B\n");
 	scanf("%d" , &b);
-	printf("Digite um lado\n");
+	printf("Digite um lado C\n");
 	scanf("%d" , &c);
-	printf("Digite um lado\n");
+	printf("Digite um lado D\n");
 	scanf("%d" , &d);
 
 	p = a+b+c/2;
 
 	if(sqrt(p * (p-a) * (p-b) * (p-c)) > 0){ // Se Maior que 0, é possivel formar um triangulo
-		printf("S/n");
+		printf("ABC formam um triangulo\n");
+	}
+	else {
+		printf("ABC nao formam um triangulo\n");
 	}
 
 	p = a+b+d/2;
 
 	if(sqrt(p * (p-a) * (p-b) * (p-d)) > 0){
-		printf("S/n");
+		printf("ABD formam um triangulo\n");
+	}
+	else {
+		printf("ABD nao formam um triangulo\n");
 	}
 
 	p = a+c+d/2;
 
 	if(sqrt(p * (p-a) * (p-c) * (p-d)) > 0){
-		printf("S/n");
+		printf("ACD formam um triangulo\n");
+	}
+	else {
+		printf("ACD nao formam um triangulo\n");
 	}
 
 	p = c+b+d/2;
 
 	if(sqrt(p * (p-c) * (p-b) * (p-d)) > 0){
-		printf("S/n");
+		printf("CBD formam um triangulo\n");
 	}
 	else{
-		printf("N\n");
+		printf("CBD nao formam um triangulo\n");
 	}
 
 	return 0;
