8ca84d140bb90f00311a05ace805762ccdbf7028
Iago Nuvem <iagonuvem@hotmail.com>
2017-06-05 19:39:48 -0300

    example/exercise 1

--- prog-1/class-05-06-2017/exercise-1.c
+++ prog-1/class-05-06-2017/exercise-1.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
-
-// Do While Example
+/* 
+Exercise:
+Read two numbers: x and y, and if x > y show the numbers in descending order from x to y,
+else if the x < y , show the numbers in ascending order from x to y. USING DO WHILE
+*/
 int main()
 {
-	int i = 1;
+	int x, y, i ,desc;
 
-	/*Its does at least one time*/
-	do{
-		printf("%d\n", i);
-		i++;
-	}while(i <= 10);
-	/*while the condition is true, loop until became false*/
+	// Read the numbers
+	printf("Digite um numero x\n");
+	scanf("%d" , &x);
+
+	printf("Digite um numero y\n");
+	scanf("%d" , &y);
+
+	// Checks which number is larger
+	if(x > y){
+		i = x;
+		printf("Ordem descrescente:\n");
+		do{
+			printf("%d\n" , i);
+			i--; // Increase the counter
+			/*
+			When increasing the counter, the value of 'desc' will be less and less,
+			Because you will subtract more from this, finding the countdown
+			*/
+		}
+		while(i >= y);
+	}
+	else if(x < y){
+		i = x; // starts the counter from the greatest number
+		printf("Ordem Ascendente:\n");
+		do{
+			printf("%d\n" , i);
+			i++; // Increase the counter
+		}
+		while(i <= y);
+	}
+	else{
+		printf("Os numeros sao iguais\n");
+	}
+	
 	return 0;
 }
