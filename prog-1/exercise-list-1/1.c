bc8c534bcf4b56cf7b04a76f1d3bfbf148568430
Iago Nuvem <iagonuvem@hotmail.com>
2017-06-13 20:47:27 -0300

    alteracoes gitignore e pasta de exercicios

---
+++ prog-1/exercise-list-1/1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+Aluno: Iago Cardoso Nuvem
+Matrícula: 17.1.8086
+Curso: Sistemas de Informação
+*/
+int main()
+{
+	int alt, bas, per, area;
+
+	printf("Digite a altura do retangulo:\n");
+	scanf("%d" , &alt);
+
+	printf("Digite a base do retangulo:\n");
+	scanf("%d" , &bas);
+
+	// Perímetro = soma dos lados , ou 2 vezes a base mais 2 vezes a altura
+	per = (2 * alt) + (2 * base);
+
+	// Área = base X Altura
+	area = bas * alt;
+
+
+	printf("Perimetro: %d\n" , per);
+	printf("Area: %d\n" , area);
+
+	return 0;
+}
