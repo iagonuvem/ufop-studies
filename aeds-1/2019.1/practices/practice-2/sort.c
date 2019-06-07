8b8f298ff79fe3b7937a97321c13abfc6ca11b20
Iago Nuvem <iagonuvem@hotmail.com>
2019-06-07 14:13:16 -0300

    shell sort

--- aeds-1/2019.1/practices/practice-2/sort.c
+++ aeds-1/2019.1/practices/practice-2/sort.c
@@ -147,7 +147,35 @@ void insertionSortO(TArray* pA,long* att,long* comp){
 }
 
 void shellSort(TArray* pA,long* att,long* comp){
-	printf("METODO NAO IMPLEMENTADO\n");
+	// Start with a big gap, then reduce the gap 
+    for (int gap = pA->n/2; gap > 0; gap /= 2) 
+    { 
+    	(*comp)++;
+        // Do a gapped insertion sort for this gap size. 
+        // The first gap elements a[0..gap-1] are already in gapped order 
+        // keep adding one more element until the entire array is 
+        // gap sorted  
+        for (int i = gap; i < pA->n; i += 1) 
+        { 
+        	(*comp)++;
+        	(*att)++;
+            // add a[i] to the elements that have been gap sorted 
+            // save a[i] in temp and make a hole at position i 
+           	int temp = pA->item[i].key; 
+  
+            // shift earlier gap-sorted elements up until the correct  
+            // location for a[i] is found 
+            int j;             
+            for (j = i; j >= gap && pA->item[j - gap].key > temp; j -= gap){
+            	(*comp)++;
+            	(*att)++;
+                pA->item[j] = pA->item[j - gap];
+            }      
+            //  put temp (the original a[i]) in its correct location 
+            (*att)++;
+            pA->item[j].key = temp; 
+        } 
+    } 
 }
 
 int partition(TArray* pA,int start, int end, long* att,long* comp){
